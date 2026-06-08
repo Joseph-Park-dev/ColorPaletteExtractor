6fd786e10a4842c5831317e2fd78fecff30f5d52
Joseph Park <jbpark1654@gmail.com>
2026-06-08 22:21:07 +0900

    COMMIT Replaced extcolors to PIL, Removed unnecessary imports

--- sample/extract_color.py
+++ sample/extract_color.py
@@ -1,16 +1,11 @@
 import math
 import os
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.image as mpimg
-
 from PIL import Image, ImageDraw
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
-
-import cv2
-import extcolors
+from collections import Counter
 
 from colormap import rgb2hex
 
@@ -27,7 +22,9 @@ def color_to_df(input):
     df = pd.DataFrame(zip(df_color_up, df_percent), columns = ['c_code','occurence'])
     return df
 
-def extract(input_image, output_image, bg_color, tolerance, limit):
+def extract(input_image, output_image, bg_color, col_count):
+    alpha_threshold = 1
+
     #background
     bg = 'bg.png'
     fig, ax = plt.subplots(figsize=(192,108),dpi=10)
@@ -38,7 +35,7 @@ def extract(input_image, output_image, bg_color, tolerance, limit):
     #resize
     resize = 1000
     output_width = resize
-    img = Image.open(input_image)
+    img = Image.open(input_image).convert("RGBA")
     if img.size[0] >= resize:
         wpercent = (output_width/float(img.size[0]))
         hsize = int((float(img.size[1])*float(wpercent)))
@@ -47,23 +44,41 @@ def extract(input_image, output_image, bg_color, tolerance, limit):
         img.save(resize_name)
     else:
         resize_name = input_image
+
+    width, height = img.size
+    total_pixels = width * height
     
-    #crate dataframe
+    #Get data
     img_url = resize_name
-    colors_x = extcolors.extract_from_path(img_url, tolerance, limit + 1)
-    df_color = color_to_df(colors_x)
-    
-    #annotate text
-    list_color = list(df_color['c_code'])
-    list_precent = [int(i) for i in list(df_color['occurence'])]
-    text_c = [c + ' ' + str(round(p*100/sum(list_precent),1)) +'%' for c, p in zip(list_color, list_precent)]
+    pixels = list(img.getdata())
+
+    # Filter out transparent pixels
+    opaque = [
+        (r, g, b)
+        for r, g, b, a in pixels
+        if a >= alpha_threshold
+    ]
+
+    if not opaque:
+        raise ValueError("No opaque pixels found in image")
+
+    counter = Counter(opaque)
+    colors = counter.most_common(col_count)    
+
+    rgbVals = list()
+    percentages = list()
+
+    for rgb, count in colors:
+        rgbVals.append(tuple(val / 255 for val in rgb))
+        percentages.append(round(count / total_pixels * 100, 1))
+
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(160,120), dpi = 10)
-    
+
     #donut plot
-    wedges, text = ax1.pie(list_precent,
-                           labels= text_c,
+    wedges, text = ax1.pie(percentages,
+                           labels= tuple(str(val) + "%" for val in percentages),
                            labeldistance= 1.05,
-                           colors = list_color,
+                           colors = rgbVals,
                            textprops={'fontsize': 150, 'color':'black'})
     plt.setp(wedges, width=0.3)
 
@@ -75,7 +90,7 @@ def extract(input_image, output_image, bg_color, tolerance, limit):
     
     #color palette
     box_width = 100
-    count = len(list_color)
+    count = len(rgbVals)
 
     x_count = 5
     y_count = math.ceil(count / x_count)
@@ -86,23 +101,25 @@ def extract(input_image, output_image, bg_color, tolerance, limit):
     for i in range (0, count):
         x = i % x_count
         y = math.floor(i / x_count)
-        draw.rectangle([(x * box_width, y * box_width), (x * box_width + box_width, y * box_width + box_width)], list_color[i])
+        fill_color = tuple(int(x * 255) for x in rgbVals[i])
+        draw.rectangle([(x * box_width, y * box_width), (x * box_width + box_width, y * box_width + box_width)], fill_color)
 
     palette.save(output_image)
     palette.show()
 
     x_posi, y_posi, y_posi2 = 160, -170, -170
-    for c in list_color:
-        if list_color.index(c) <= 5:
+    for c in rgbVals:
+        color_str = tuple(round(x * 100, 1) for x in c)
+        if rgbVals.index(c) <= 5:
             y_posi += 180
             rect = patches.Rectangle((x_posi, y_posi), 360, 160, facecolor = c)
             ax2.add_patch(rect)
-            ax2.text(x = x_posi+400, y = y_posi+100, s = c, fontdict={'fontsize': 190})
+            ax2.text(x = x_posi+400, y = y_posi+100, s = color_str, fontdict={'fontsize': 190})
         else:
             y_posi2 += 180
             rect = patches.Rectangle((x_posi + 1000, y_posi2), 360, 160, facecolor = c)
             ax2.add_artist(rect)
-            ax2.text(x = x_posi+1400, y = y_posi2+100, s = c, fontdict={'fontsize': 190})
+            ax2.text(x = x_posi+1400, y = y_posi2+100, s = color_str, fontdict={'fontsize': 190})
 
     fig.set_facecolor('white')
     ax2.axis('off')
